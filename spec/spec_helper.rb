c570c7fdecc205b6e56bd26c30e7f3f5e0230a6e
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-19 20:43:46 +0900

    Fix spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,51 @@
+require 'tempfile'
 require 'miam'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    apply { '' }
+  end
+end
+
+def client(user_options = {})
+  options = {logger: Logger.new('/dev/null')}
+
+  if_debug do
+    logger = Miam::Logger.instance
+    logger.set_debug(true)
+
+    options.update(
+      debug: true,
+      logger: logger,
+      aws_config: {
+        http_wire_trace: true,
+        logger: logger
+      }
+    )
+  end
+
+  options = options.merge(user_options)
+  Miam::Client.new(options)
+end
+
+def tempfile(content, options = {})
+  basename = "#{File.basename __FILE__}.#{$$}"
+  basename = [basename, options[:ext]] if options[:ext]
+
+  Tempfile.open(basename) do |f|
+    f.puts(content)
+    f.flush
+    f.rewind
+    yield(f)
+  end
+end
+
+def apply(cli = client)
+  tempfile(yield) do |f|
+    cli.apply(f.path)
+  end
+end
+
+def if_debug
+  yield if ENV['DEBUG'] == '1'
+end
