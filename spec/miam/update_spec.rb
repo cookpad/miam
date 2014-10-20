0a166149cce001cca1807792173fb8a5afd4761d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-20 09:22:55 +0900

    Fix update spec

--- spec/miam/update_spec.rb
+++ spec/miam/update_spec.rb
@@ -185,6 +185,32 @@ describe 'update' do
               [{"Effect"=>"Allow", "Action"=>"*", "Resource"=>"*"}]}
           end
         end
+
+        role "my-role", :path=>"/any/" do
+          instance_profiles(
+            "my-instance-profile"
+          )
+
+          assume_role_policy_document do
+            {"Version"=>"2012-10-17",
+             "Statement"=>
+              [{"Sid"=>"",
+                "Effect"=>"Allow",
+                "Principal"=>{"Service"=>"ec2.amazonaws.com"},
+                "Action"=>"sts:AssumeRole"}]}
+          end
+
+          policy "role-policy" do
+            {"Statement"=>
+              [{"Action"=>
+                 ["s3:Get*",
+                  "s3:List*"],
+                "Effect"=>"Allow",
+                "Resource"=>"*"}]}
+          end
+        end
+
+        instance_profile "my-instance-profile", :path=>"/profile/"
       RUBY
     end
 
