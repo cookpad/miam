ba0f7a6a0da9c1ffc41e0a1b5b2dc183264e9a26
Kohei Suzuki <kohei-suzuki@cookpad.com>
2023-06-08 18:41:48 +0900

    Reorder Hash keys for trust policy comparison

--- spec/miam/hash_ext_spec.rb
+++ spec/miam/hash_ext_spec.rb
@@ -36,4 +36,71 @@ describe 'Hash#sort_array!' do
   subject { hash.sort_array! }
 
   it { is_expected.to eq expected_hash }
+
+  context 'on trust policy' do
+    let(:expected_trust_policy) do
+      {
+	'Version' => '2012-10-17',
+	'Statement' => [
+          {
+            'Action' => 'sts:AssumeRole',
+            'Effect' => 'Allow',
+            'Principal' => {
+              'AWS' => 'arn:aws:iam::111122223333:role/Role1',
+            },
+            'Sid' => 'sid1',
+          },
+          {
+            'Effect' => 'Allow',
+            'Principal' => {
+              'Federated' => 'arn:aws:iam::111122223333:oidc-provider/oidc.eks.ap-northeast-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B71EXAMPLE',
+            },
+            'Action' => 'sts:AssumeRoleWithWebIdentity',
+            'Condition' => {
+              'StringEquals' => {
+                'oidc.eks.ap-northeast-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B71EXAMPLE:sub' => 'system:serviceaccount:default:miam',
+                'oidc.eks.ap-northeast-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B71EXAMPLE:aud' => 'sts.amazonaws.com',
+              },
+            },
+          },
+        ],
+      }
+    end
+
+    let(:actual_trust_policy) do
+      {
+        'Version' => '2012-10-17',
+        'Statement' => [
+          {
+            # Only the order of key-value pairs below are different
+            'Sid' => 'sid1',
+            'Effect' => 'Allow',
+            'Principal' => {
+              'AWS' => 'arn:aws:iam::111122223333:role/Role1',
+            },
+            'Action' => 'sts:AssumeRole',
+          },
+          {
+            'Effect' => 'Allow',
+            'Principal' => {
+              'Federated' => 'arn:aws:iam::111122223333:oidc-provider/oidc.eks.ap-northeast-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B71EXAMPLE',
+            },
+            'Action' => 'sts:AssumeRoleWithWebIdentity',
+            'Condition' => {
+              'StringEquals' => {
+                'oidc.eks.ap-northeast-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B71EXAMPLE:sub' => 'system:serviceaccount:default:miam',
+                'oidc.eks.ap-northeast-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B71EXAMPLE:aud' => 'sts.amazonaws.com',
+              },
+            },
+          },
+        ],
+      }
+    end
+
+    it 'ignores the order of Hash entries' do
+      expected_trust_policy.sort_array!
+      actual_trust_policy.sort_array!
+      expect(expected_trust_policy).to eq(actual_trust_policy)
+    end
+  end
 end
