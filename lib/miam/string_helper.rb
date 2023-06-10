921b9802204e5398b5761306834b7d561265b2a8
Kohei Suzuki <eagletmt@gmail.com>
2023-06-10 17:04:21 +0900

    Do not overwrite String core methods
    Term::ANSIColor::Attribute contains attribute names which conflict with
    String core methods: `clear` and `reverse`. Overriding those core
    methods breaks IRB and Pry (and other possible gems). It can be
    reproduced by running `bundle exec ruby -e 'require "miam"; binding.irb'`.
    This makes Miam debugging hard for me.

---
+++ lib/miam/string_helper.rb
@@ -0,0 +1,25 @@
+module Miam
+  module StringHelper
+    @colorize = false
+
+    class << self
+      def colorize=(value)
+        @colorize = value
+      end
+
+      def colorize
+        @colorize
+      end
+
+      Term::ANSIColor::Attribute.named_attributes.map do |attribute|
+        define_method(attribute.name) do |str|
+          if colorize
+            Term::ANSIColor.public_send(attribute.name, str)
+          else
+            str
+          end
+        end
+      end
+    end
+  end
+end
